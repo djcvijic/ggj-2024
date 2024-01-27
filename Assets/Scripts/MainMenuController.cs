093726e4fe46b0c3b69c62dbf78d113e915da3dd
Sanja15513 <110911747+Sanja15513@users.noreply.github.com>
2024-01-27 23:37:26 +0100

    Main menu effects finished

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,6 +7,8 @@ public class MainMenuController : MonoBehaviour
 {
     public Button commedianButton;
     public Button whispererButton;
+    public Button commedianClickButton;
+    public Button whispererClickButton;
     public Button shareButton;
     public Image shareQRImage;
 
@@ -14,7 +16,9 @@ public class MainMenuController : MonoBehaviour
     {
         commedianButton.onClick.AddListener(() => LoadScene(PlayerType.Catmedian));
         whispererButton.onClick.AddListener(() => LoadScene(PlayerType.CatWhisperer));
-        shareButton.onClick.AddListener(() => OpenSharePopup());
+        commedianClickButton.onClick.AddListener(() => SwitchCatButton(PlayerType.Catmedian));
+        whispererClickButton.onClick.AddListener(() => SwitchCatButton(PlayerType.CatWhisperer));
+        //shareButton.onClick.AddListener(() => OpenSharePopup());
     }
 
     public void LoadScene(PlayerType playerType)
@@ -28,6 +32,21 @@ public class MainMenuController : MonoBehaviour
             SceneManager.LoadScene("SceneWhisperer");
         }
     }
+
+    public void SwitchCatButton(PlayerType playerType)
+    {
+        if (playerType == PlayerType.Catmedian)
+        {
+            commedianButton.gameObject.SetActive(true);
+            whispererButton.gameObject.SetActive(false);
+        }
+        else if (playerType == PlayerType.CatWhisperer)
+        {
+            commedianButton.gameObject.SetActive(false);
+            whispererButton.gameObject.SetActive(true);
+        }
+    }
+
     private void OpenSharePopup()
     {
         shareQRImage.gameObject.SetActive(true);
